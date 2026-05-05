9d7faa00de06f3ab20e1122e0b1216809f9545ef
slacangan <slacangan@gmail.com>
2026-05-05 10:49:40 -0700

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,6 +1,8 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./style.css";
-
-createRoot(document.getElementById("root")).render(<App />);
+export default function App() {
+  return (
+    <div style={{ padding: 40 }}>
+      <h1>Curious and Rooted 🌿</h1>
+      <p>Your app is live.</p>
+    </div>
+  );
+}
